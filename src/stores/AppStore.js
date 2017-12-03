90cb02aa6cd10a3ab0262b05764db1600a411c5d
Jega <gatomzl@gmail.com>
2017-12-03 02:52:22 -0500

    React desde 0 - users - Implementacion de Reflux en el home

--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -2,11 +2,19 @@ import Reflux from 'reflux';
 import Actions from '../actions/Actions.js';
 
 class AppStore extends Reflux.Store {
-    constructor()[
+    constructor() {
         super();
         // link de las acciones disponibles en el archivo actions.js
         this.listenables = Actions;
-    ]
+        // Estado que va a ser leido por el home
+        this.state = {
+            data: []
+        }
+    }
+
+    getUsers() {
+        alert("get_users");
+    }
 }
 
 export default AppStore;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,16 +1,33 @@
 import React, {Component} from 'react';
+import Reflux from 'reflux';
+import AppStore from '../stores/AppStore';
+import Actions from '../actions/Actions';
 import '../styles/Home.css';
 
-class Home extends Component {
+class Home extends Reflux.Component {
     constructor(){
         super();
+        // Declaramos state para que pueda acceder a los stados en la store
+        this.state = {};
+        // Conectamos con la store
+        this.store = AppStore;
+        // Variables de estado que se van a compartir entre el store y el home
+        this.storeKeys = ['data'];
+        this.showUsers = this.showUsers.bind(this);
     }
+
+    showUsers(e){
+        Actions.getUsers();
+    }
+
     render() {
         return (
             <div className="Home">
-                <div className="home-header-box">
+                <div className="Home-header-box">
                     <h2 className="Home-header-title">Bienvenido</h2>
-                    <button className="Home-header-button">Mostar Usuarios</button>
+                    <button
+                        onClick={ this.showUsers }
+                        className="Home-header-button">Mostar Usuarios</button>
                 </div>
             </div>
         );
